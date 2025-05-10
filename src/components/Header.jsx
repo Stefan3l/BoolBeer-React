ca89059f1072e4335e0c955efb1074c8f528b12f
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-05-10 21:37:22 +0200

    feat: add efetto scroll header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,32 +13,42 @@ export default function Header() {
   }, []);
 
   return (
-    <div
-      className={`fixed w-full z-50 flex justify-between items-center py-5 transition-all duration-300 text-white ${
-        isScrolled ? "bg-[#0D2227]" : "bg-transparent"
-      }`}
-    >
-      <div>
-        <img
-          src="../images/logo-beer.png"
-          alt="Logo"
-          className={`transition-all duration-300 ${
-            isScrolled ? "w-36" : "w-32"
-          }`}
-        />
-      </div>
-      <div>
-        <ul className="flex list-unstyled gap-4 mr-4">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
+    <div className="fixed w-full z-[40]">
+      <div
+        className={`w-full transition-all duration-300 ${
+          isScrolled
+            ? "bg-[#0D2227] h-[120px] clip-header relative"
+            : "bg-transparent h-[100px]"
+        }`}
+      >
+        <div className="flex justify-between items-start py-3 px-4 h-full text-white">
+          <div
+            className={`absolute left-4 z-[60] ${
+              isScrolled ? "-top-2" : "top-0"
+            }`}
+          >
+            <img
+              src="../images/logo-beer.png"
+              alt="Logo"
+              className={`transition-all duration-300 ${
+                isScrolled ? "w-64" : "w-32"
+              }`}
+            />
+          </div>
+          <div className={`ml-auto ${isScrolled ? "mt-10" : ""}`}>
+            <ul className="flex list-unstyled gap-4 mr-4">
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
