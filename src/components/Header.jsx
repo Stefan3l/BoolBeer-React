7d703e85101e5fc01961beb6ac5b4a5847107a7e
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-05-10 21:18:47 +0200

    fix:header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,31 @@
+import { useState, useEffect } from "react";
+
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="flex justify-between items-center py-5 bg-[#0D2227] text-white ">
+    <div
+      className={`fixed w-full z-50 flex justify-between items-center py-5 transition-all duration-300 text-white ${
+        isScrolled ? "bg-[#0D2227]" : "bg-transparent"
+      }`}
+    >
       <div>
-        <img src="../images/logo-beer.png" alt="Logo" className="w-32" />
+        <img
+          src="../images/logo-beer.png"
+          alt="Logo"
+          className={`transition-all duration-300 ${
+            isScrolled ? "w-36" : "w-32"
+          }`}
+        />
       </div>
       <div>
         <ul className="flex list-unstyled gap-4 mr-4">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,48 @@
-//import components
+import Header from "../components/Header";
 import Hero from "../components/ui/Hero";
 
 export default function HomePage() {
   return (
-    <>
+    <div className="relative">
+      <Header />
+
       <Hero />
-    </>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora in
+        provident temporibus dolor autem, culpa tempore est repudiandae laborum?
+        Illo quas placeat voluptas officiis nam beatae? Cupiditate dolor quidem
+        iste. Fugiat quasi dignissimos, mollitia obcaecati nesciunt, at voluptas
+        exercitationem numquam quas excepturi necessitatibus incidunt, enim
+        nostrum! Adipisci necessitatibus repellat nostrum voluptate, porro
+        magni, qui delectus, odit quas quia voluptates culpa. Dignissimos eius
+        laborum cupiditate molestiae amet eaque facere expedita fugiat, libero
+        aspernatur quibusdam et! Ab at fugit atque totam laborum, temporibus
+        odio repudiandae! Mollitia, vitae minima? Tempora numquam architecto
+        blanditiis. Quaerat accusantium nam sequi praesentium, labore ut iusto
+        id animi ipsa illum. Ipsum, beatae quasi doloremque eveniet fuga vel
+        sapiente libero repudiandae id veniam quos assumenda dicta
+        necessitatibus, earum harum! Temporibus nihil minus cum est. Alias
+        voluptates ducimus perferendis quas eum error delectus quia aperiam
+        odio, nam, quam illum placeat eveniet, officiis sunt expedita sint
+        doloribus eligendi maxime impedit. Fuga. Nihil, nemo commodi laborum,
+        aliquam, sequi eaque facere natus dolores rerum fugiat cupiditate
+        aliquid maxime ad expedita at minima et eum tempore molestiae quae non
+        perspiciatis. Culpa reprehenderit minima excepturi? Esse pariatur facere
+        laborum architecto corrupti dolorum, dolore quidem ab voluptas
+        aspernatur aliquam ducimus? Consequuntur qui ea deserunt cum possimus
+        repellendus, inventore repellat, doloremque libero minima vero
+        voluptates, eum ipsa. Totam, molestias dolorem ad rerum aliquid
+        distinctio praesentium fuga mollitia perspiciatis, labore laborum illum
+        magnam quo culpa rem aspernatur suscipit nobis, quas est ipsa debitis
+        assumenda magni. Temporibus, consectetur totam! Nam commodi nisi tempora
+        harum? Id debitis alias vitae doloremque adipisci iste quaerat
+        perspiciatis laborum. Deserunt, perferendis ipsam dolores, doloribus
+        quisquam voluptatum natus cumque maxime magnam, sit ratione illo
+        praesentium! Officiis excepturi odio exercitationem soluta fugit! Eum,
+        nihil. Maxime eius deserunt molestiae sed nam nesciunt magnam natus
+        autem sit tempora, aliquam hic reprehenderit cupiditate placeat nihil
+        vel dignissimos iusto non?
+      </p>
+    </div>
   );
 }
